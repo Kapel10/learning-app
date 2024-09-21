03690372036c8423f0f555d3a078db85b587d003
rakhymzhan <Rahimzhan_T@mail.ru>
2024-09-21 12:31:51 +0500

    first changes

--- learning-app/src/page/main/Main.tsx
+++ learning-app/src/page/main/Main.tsx
@@ -4,7 +4,7 @@ const Main = () => {
   return (
     <div className="w-screen h-screen">
       <SideBar />
-      <div className="text-yellow-500">1</div>
+      <div className="text-yellow-500">some text</div>
     </div>
   )
 }
